82d60d1c9df6b3cd5b1ad15d62c9159fadbb3ed5
ethel <paulsasieta.upv@gmail.com>
2022-04-22 20:04:57 +0200

    Input parameter setup

--- src/main.py
+++ src/main.py
@@ -5,12 +5,28 @@ sys.path.insert(0, str(os.getcwd()))
 
 import os.path as path
 import javaproperties
+import json
 
 import src.config.config as config
+import src.logger.logger as logger
+import src.validator as validator
 
 
 def main(configs: config.Config):
-    print('main')
+
+    logging_service = logger.Logger(name='event-rule-frequency-logger', path=configs.get("logging.path"),
+                                        level=configs.get("logging.level"))
+
+    logging_service.info("============== SNAKE BOARD GAME DEPTH POSSIBILITIES CALCULATOR LAUNCH ==============")
+
+    try:
+        input_args_board = validator.validate_board(configs.get("input.depth"))
+        input_args_depth = validator.validate_depth(configs.get("input.depth"))
+        input_args_snake = validator.validate_snake(configs.get("input.snake"))
+
+    except validator.ValidationError as e:
+        logging_service.error(e)
+        logging_service.info("============== EXECUTION ABORTED ==============")
 
 
 if __name__ == '__main__':
